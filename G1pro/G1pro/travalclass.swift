288a11d0bec9305a0a99852f36ea87fbf0bd4cbe
kimjiwon <jiwon17ar@naver.com>
2016-10-07 10:56:03 +0900

    team1
    세계여행

---
+++ G1pro/G1pro/travalclass.swift
@@ -0,0 +1,15 @@
+//
+//  travalclass.swift
+//  G1pro
+//
+//  Created by Mac19 on 2016. 10. 7..
+//  Copyright © 2016년 Mac19. All rights reserved.
+//
+
+import UIKit
+
+class travalclass: UITableViewController {
+
+    var  
+    
+}
---
+++ G1pro/TVTableViewController.swift
@@ -0,0 +1,107 @@
+//
+//  TVTableViewController.swift
+//  G1pro
+//
+//  Created by Mac19 on 2016. 10. 7..
+//  Copyright © 2016년 Mac19. All rights reserved.
+//
+
+import UIKit
+
+class TVTableViewController: UITableViewController {
+
+    var trip:[String:Int] = ["인도":37,"미국":20,"중국":15,"일본":10,"베트남":8,"라오스":7,"레바논":18,"홍콩":15,"러시아":4,"태국":4]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+       
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return trip.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+
+        
+        let tripnames = Array(trip.keys)
+        let tripname = tripnames[indexPath.row]
+        cell.textLabel!.text = tripname
+        
+        if let capacity:Int = trip[tripname]
+        {
+            cell.detailTextLabel?.text = "\(capacity)"
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
